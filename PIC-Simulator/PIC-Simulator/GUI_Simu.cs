a42aa382c7ab9c76b4636da684496903519055a2
lange98 <lange.dominik@student.dhbw-karlsuhe.de>
2020-04-23 11:51:39 +0200

    connected parser to GUI

--- PIC-Simulator/PIC-Simulator/GUI_Simu.cs
+++ PIC-Simulator/PIC-Simulator/GUI_Simu.cs
@@ -8,10 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace PIC_Simulator
 {
     public partial class GUI_Simu : Form
     {
+        #region variables
+        Parser parser = new Parser();
+        List<int> commands = new List<int>();
+        #endregion
+
         public GUI_Simu()
         {
             InitializeComponent();
@@ -21,7 +27,11 @@ namespace PIC_Simulator
         #region Control-Buttons
         private void btnStart_Click(object sender, EventArgs e)
         {
-            
+            commands = parser.parseFile();
+            for (int i = 0; i < commands.Count; i++)
+            {
+                tBProgramm.AppendText(commands[1].ToString());
+            }
         }
 
         private void btnStop_Click(object sender, EventArgs e)
