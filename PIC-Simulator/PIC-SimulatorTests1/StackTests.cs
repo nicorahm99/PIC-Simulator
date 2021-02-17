b37a0c9ed8bc262a786db8a40ffe9341a5acb1f1
Nico Rahm <nicorahm99@gmx.de>
2021-02-17 09:01:19 +0100

    added Rom and Stack Tests

--- PIC-Simulator/PIC-SimulatorTests1/StackTests.cs
+++ PIC-Simulator/PIC-SimulatorTests1/StackTests.cs
@@ -8,13 +8,51 @@ using System.Threading.Tasks;
 
 namespace PIC_Simulator.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class StackTests
     {
-        [Test()]
-        public void pushTest()
+        int VALID_INTEGER = 12345;
+
+        [Test]
+        public void singlePushPopTest()
         {
-            Assert.Fail();
+            var classUnderTest = getClassUnderTest();
+    
+            classUnderTest.push(VALID_INTEGER);
+
+            Assert.That(classUnderTest.pop().Equals(VALID_INTEGER));
+        }
+
+        [Test]
+        public void multiplePushPopTest()
+        {
+            var classUnderTest = getClassUnderTest();
+
+            fillStackUntilOverflowOccurs(classUnderTest);
+
+            Assert.That(lastElementIn(classUnderTest).Equals(8));
+        }
+
+        private object lastElementIn(Stack classUnderTest)
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                classUnderTest.pop();
+            }
+            return classUnderTest.pop();
+        }
+
+        private void fillStackUntilOverflowOccurs(Stack classUnderTest)
+        {
+            for(int i = 0; i <= 8; i++)
+            {
+                classUnderTest.push(i);
+            }
+        }
+
+        private Stack getClassUnderTest()
+        {
+            return new Stack();
         }
     }
 }
